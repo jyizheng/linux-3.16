688db951563ad1421928f63ad957e50323ae9b8a
jyizheng <jyizheng@gmail.com>
2016-01-31 20:16:11 -0500

    add vmstate.c

--- mm/vmstat.c
+++ mm/vmstat.c
@@ -885,7 +885,7 @@ static void frag_show_print(struct seq_file *m, pg_data_t *pgdat,
 
 	seq_printf(m, "Node %d, zone %8s ", pgdat->node_id, zone->name);
 	for (order = 0; order < MAX_ORDER; ++order)
-		seq_printf(m, "%6lu ", zone->free_area[order].nr_free);
+		seq_printf(m, "%4lu ", zone->free_area[order].nr_free);
 	seq_putc(m, '\n');
 }
 
